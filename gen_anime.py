79b052ec72a50f1f85a0fd9f5bccbf0aeaae94a8
dady_ocat@hotmail.com <dady_ocat@hotmail.com>
2022-09-11 18:26:29 +0700

    generator mcmeta for image size

---
+++ gen_anime.py
@@ -0,0 +1,26 @@
+import os
+import cv2
+
+for file in os.listdir('textures'):
+    for file2 in os.listdir('textures/' + file):
+        # shutil.copy('textures/'+file+'/' + file2,'textures/'+file+'/' + file2.replace('.png','_e.png'))
+        im = cv2.imread('textures/'+file+'/' + file2)
+        h, w, c = im.shape
+        if h != w:
+            i = int(h/ w)
+            print(i)
+            # for x in range(i):
+            #     print(x)
+            with open(f"textures/{file}/{file2}.mcmeta", "w") as f:
+                f.write(f'{{')
+                f.write(f'\n\t"animation": {{')
+                f.write(f'\n\t"frametime": 4,')
+                f.write(f'\n\t"frames": [')
+                for x in range(i):
+                    f.write(f'\n\t\t{x}')
+                    if x != i-1:
+                        f.write(f',')
+                f.write(f'\n\t]')
+                f.write(f"\n  }}")
+                f.write(f"\n}}")
+
